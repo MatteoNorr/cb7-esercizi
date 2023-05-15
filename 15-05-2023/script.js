4608a114c1a9fcf1065a29a8fd71b2e3d00d2919
Matt <131680253+MatteoNorr@users.noreply.github.com>
2023-05-15 18:10:35 +0200

    Delete 15-05-2023 directory

--- 15-05-2023/script.js
+++
@@ -1,9 +0,0 @@
-import { cE, qS, addProduct } from "./utils/fn.js";
-
-const mainEl = qS("#main");
-
-fetch("https://dummyjson.com/products")
-  .then((res) => res.json())
-  .then((data) =>
-    data.products.forEach((product) => mainEl.append(addProduct(product)))
-  );
