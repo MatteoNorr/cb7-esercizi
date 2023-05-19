ba2dbc8cad50c4388538b8900e3a6f5f9a834826
Matt <131680253+MatteoNorr@users.noreply.github.com>
2023-05-19 17:43:02 +0200

    Esercitazione 19 maggio 2023

---
+++ 19-05-2023/utils/credentials.js
@@ -0,0 +1,8 @@
+export const users = [
+  {
+    username: "Matteo",
+    password: "cavallo",
+  },
+  { username: "Mario", password: "cavalli" },
+  { username: "Marco", password: "cavallo2" },
+];
